1b3558e810440d44108f539c2dd621d69c9bcf78
Jihye Seo <rabierre@gmail.com>
2016-10-20 09:37:16 +0900

    Show label name

--- models/record.go
+++ models/record.go
@@ -2,6 +2,8 @@ package models
 
 import (
 	"time"
+
+	"github.com/rabierre/scrooge/db"
 )
 
 type Record struct {
@@ -9,4 +11,12 @@ type Record struct {
 	Time    time.Time
 	Amount  string
 	LabelId uint64
+}
+
+func (r *Record) LabelName() string {
+	obj, err := db.Dbm.Get(Label{}, r.LabelId)
+	if err != nil {
+		panic(err)
+	}
+	return obj.(*Label).Name
 }
